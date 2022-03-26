0ea4236ef496715880f7e9eb7f0db430b2024992
Margherita Parimbelli <margheritaparimbelli@pc-14-46.customer.ask4.lan>
2022-03-26 20:22:52 +0000

    Modified top functions and test area in Vandam's code

--- Rough/Margherita.py
+++ Rough/Margherita.py
@@ -10,22 +10,46 @@ import pandas as pd
 import os
 import plotly.express as px
 
-for f in os.listdir('/Users/margheritaparimbelli/Documents/FP_Assignment/'):
-    print(f)
+df = pd.read_csv('https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&areaCode=S12000033&metric=cumCasesByPublishDate&metric=cumPeopleVaccinatedFirstDoseByVaccinationDate&metric=cumPeopleVaccinatedSecondDoseByVaccinationDate&metric=cumPeopleVaccinatedThirdInjectionByVaccinationDate&format=csv')
+print(df)
 
-data = pd.read_csv ('/Users/margheritaparimbelli/Documents/FP_Assignment/BristolVacc.csv', sep = ',')
+start_date = input('Input desired start date as follows(YYYY-MM-DD):' )
+end_date = input('Input desired end date as follows(YYYY-MM-DD):' )
 
-df = px.data.gapminder().query()
+#date = (start_date <= df['date']) & (df['date'] <= end_date)
+    
+# Show fig, x axis, y axis 
+fig = px.line(df, x = 'date', y = ['cumCasesByPublishDate','cumPeopleVaccinatedFirstDoseByVaccinationDate','cumPeopleVaccinatedSecondDoseByVaccinationDate','cumPeopleVaccinatedThirdInjectionByVaccinationDate'])
+fig.update_layout(xaxis_range=[start_date,end_date])
+fig.show()
 
-x = data['date']
-y = data['cumCasesByPublishDate']
 
-data.plot(x, y)
 
-plt.title('Cases according to dates', fontsize = 12)
-plt.xlabel('Dates', fontsize = 12)
-plt.ylabel('Cases numbers', fontsize = 12)
-plt.grid(True)
+
+
+
+
+
+# df = pd.read_csv('https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&metric=cumCasesBySpecimenDate&metric=cumPeopleVaccinatedFirstDoseByVaccinationDate&metric=cumPeopleVaccinatedSecondDoseByVaccinationDate&metric=cumPeopleVaccinatedThirdInjectionByVaccinationDate&format=csv')
+# print(df)
+
+
+# for f in os.listdir('/Users/margheritaparimbelli/Documents/FP_Assignment/'):
+#     print(f)
+
+# data = pd.read_csv ('/Users/margheritaparimbelli/Documents/FP_Assignment/BristolVacc.csv', sep = ',')
+
+# df = px.data.gapminder().query()
+
+# x = data['date']
+# y = data['cumCasesByPublishDate']
+
+# data.plot(x, y)
+
+# plt.title('Cases according to dates', fontsize = 12)
+# plt.xlabel('Dates', fontsize = 12)
+# plt.ylabel('Cases numbers', fontsize = 12)
+# plt.grid(True)
 
 # print(df)
 
