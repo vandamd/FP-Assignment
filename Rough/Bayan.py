13951987fc1e75447470b4c242574c5d6f6e16b3
cloud-26 <cy21707@bristol.ac.uk>
2022-04-09 04:15:03 +0200

    added loops to output data for multiple locations

--- Rough/Bayan.py
+++ Rough/Bayan.py
@@ -131,191 +131,211 @@ def timeRange():
 
 
 # Generates Graph
-def graphgen(userLocationsList):
-    """
-      - Determines the number of cities entered
-      - If there is one city entered, generates a graph of covid cases
-      - If there is more than one city entered, poop is outputted
-    """
-    # The number of words in userLocations
-    N = len(userLocationsList)
-    
-    # Instance for 1 Location inputted
-    if N == 1:
-        df = pd.read_csv(url3)
-        df_filtered = df[df['areaName'].str.contains(r'\b' + userLocationsList[0] + r'\b')]
-        df_reset = df_filtered.reset_index(drop=True)
+def graphgen():
+            """
+              - Determines the number of cities entered
+              - If there is one city entered, generates a graph of covid cases
+              - If there is more than one city entered, poop is outputted
+            """
+            # The number of words in userLocations
+            #N = len(userLocationsList)
+            
+            # Instance for 1 Location inputted
+            #if N == 1:
+            
+            global userLocationsList
         
-        
-        first_dose_people = df_reset.loc[0,'cumPeopleVaccinatedFirstDoseByVaccinationDate']
-        second_dose_people = df_reset.loc[0,'cumPeopleVaccinatedSecondDoseByVaccinationDate']
-        third_dose_people = df_reset.loc[0,'cumPeopleVaccinatedThirdInjectionByVaccinationDate']
-        values = [first_dose_people, second_dose_people, third_dose_people]
-        
-        
-        
-        ### ACTUAL GRAPH GEN PART ###
-        
-        
-        # Subplots
-        fig = make_subplots(
-            rows=2, cols=2,
-            shared_xaxes=True,
-            vertical_spacing=0.03,
-            column_widths=[1, 0.4],
-            # row_heights=[0.2, 1],
-            specs=[[{"secondary_y": True}, {"type": "pie"}],
-                   [{"type": "scatter"}, {"type": "choropleth"}]])
-        
-        fig.add_trace(
-            go.Scatter(
-                x=df_filtered["date"],
-                y=df_filtered['cumCasesBySpecimenDate'],
-                mode="lines",
-                name="COVID-19 Cases",
-            ),
-            row=1, col=1
-        )
-        
-        fig.add_trace(
-            go.Scatter(
-                x=df_filtered["date"], 
-                y=df_filtered['newCasesBySpecimenDate'], 
-                name="New COVID-19 Cases"),
-            secondary_y=True,
-            row=1, col=1
-        )
-        
-        fig.add_trace(
-            go.Scatter(
-                x=df_filtered["date"],
-                y=df_filtered['cumPeopleVaccinatedFirstDoseByVaccinationDate'],
-                mode="lines",
-                name="First Dose"
-            ),
-            row=2, col=1
-        )
-
-        fig.add_trace(
-            go.Scatter(
-                x=df_filtered["date"], 
-                y=df_filtered['cumPeopleVaccinatedSecondDoseByVaccinationDate'], 
-                name="Second Dose"),
-            row=2, col=1
-        )
-
-        fig.add_trace(
-            go.Scatter(
-                x=df_filtered["date"], 
-                y=df_filtered['cumPeopleVaccinatedThirdInjectionByVaccinationDate'], 
-                name="Third Dose"),
-            row=2, col=1
-        )
-        
-        
-
-
-
-        fig.add_trace(
-            go.Pie(
-                labels=['First Dose','Second Dose', 'Third Injection'], 
-                values=values, 
-                textinfo='label+percent', 
-                pull=[0,0,0],
-                name="Vaccine Doses"
-            ), 
-            row=1, col=2
-        )
-        
-        
-        
-        
-        fig.add_trace(          # GEOGRAPHICAL MAP
-            go.Choropleth(
-                geojson = '/Users/vandam/Library/Mobile Documents/com~apple~CloudDocs/Documents/School/Year 1/Programming/Documents/Further Programming/FP-Assignment/Rough/lad.json',
-                locationmode = 'geojson-id',
-                # featureidkey='properties.LAD13NM',
-                # locations=df_filtered["areaName"],
-                # color=df_filtered["cumCasesBySpecimenDate"],
-                # color_continuous_scale='Reds',
-                # locationmode = 'geojson-id',
-                # color='cumCasesBySpecimenDate',
-                # color=df_filtered['cumCasesBySpecimenDate'],
-                # hover_name=df_filtered["areaName"],
-                # animation_frame=df_filtered["date"],
-                name="COVID-19 Cases",
-            ),
-            row=2, col=2
-        )
-        
-        
-        
-        
-        
-        
-        
-        fig.update_yaxes(title_text="Cumulative COVID-19 Cases", row=1, col=1)
-        fig.update_yaxes(title_text="New Cases", row=1, col=1, secondary_y=True)
-        fig.update_yaxes(title_text="Vaccine Doses", row=2, col=1)
-        
-        fig.update_xaxes(title_text="", row=1, col=1)
-        fig.update_xaxes(title_text="Date", row=2, col=1)
-        
-        fig.update_layout(title_text="COVID-19 Data")
-        fig.update_layout(xaxis_range=[start_date, end_date])
-        # fig.update_geos(scope='europe') 
-        
-        fig.update_layout(
-            xaxis=dict(
-                rangeselector=dict(
-                    buttons=list(
-                        [
-                            dict(count=1, label="1m", step="month", stepmode="backward"),
-                            dict(count=6, label="6m", step="month", stepmode="backward"),
-                            dict(count=1, label="1y", step="year", stepmode="backward"),
-                            dict(step="all"),
-                        ]
-                    )
-                ),
-                type="date",
-            ),
-            xaxis2=dict(
-                rangeslider=dict(visible=True),
-                type="date",
-            ),
-        )
-        fig.update_xaxes(matches='x')
-        fig.show()
-
-        
-        
-        
-        
-        # # Line Chart
-        # fig1 = px.line(df_filtered, x = 'date', 
-        #                y = ['cumCasesBySpecimenDate',
-        #                     'cumPeopleVaccinatedFirstDoseByVaccinationDate', 
-        #                     'cumPeopleVaccinatedSecondDoseByVaccinationDate',
-        #                     'cumPeopleVaccinatedThirdInjectionByVaccinationDate'], 
-        #                labels=dict(x="Date", y="Number of people"))
-        # fig1.update_layout(xaxis_range=[start_date, end_date])
-        # fig1.show()
-        
-        # # Pie Chart
-        # first_dose_people = df_reset.loc[0,'cumPeopleVaccinatedFirstDoseByVaccinationDate']
-        # second_dose_people = df_reset.loc[0,'cumPeopleVaccinatedSecondDoseByVaccinationDate']
-        # third_dose_people = df_reset.loc[0,'cumPeopleVaccinatedThirdInjectionByVaccinationDate']
-        # values = [first_dose_people, second_dose_people, third_dose_people]
+            for location in userLocationsList:
                 
-        # fig2 = go.Figure(data=[go.Pie(labels=['First Dose','Second Dose', 'Third Injection'], values=values, textinfo='label+percent', pull=[0.1,0.1,0.1])])
-        # fig2.show()
-        
-        
-    elif N > 1:
-        print('poop')
-
-
-
+                if location == userLocationsList[-1]:
+                    break 
+                '''
+            - loops through every location in the list
+            - supposed to break the loop when it reaches the last location
+            '''
+                
+                for index in range(len(userLocationsList)):
+                    if index == len(userLocationsList):
+                        break
+                    ''''
+                    - iterates through the indices of locations in the list to use in df_filtered (userLocationsList[index])
+                    - supposed to break when it reaches the last location 
+                    '''
+                    
+                    
+                    df = pd.read_csv(url3)
+                    df_filtered = df[df['areaName'].str.contains(r'\b' + userLocationsList[index] + r'\b')]
+                    df_reset = df_filtered.reset_index(drop=True)
+                    
+                    
+                    first_dose_people = df_reset.loc[index,'cumPeopleVaccinatedFirstDoseByVaccinationDate']
+                    second_dose_people = df_reset.loc[index,'cumPeopleVaccinatedSecondDoseByVaccinationDate']
+                    third_dose_people = df_reset.loc[index,'cumPeopleVaccinatedThirdInjectionByVaccinationDate']
+                    values = [first_dose_people, second_dose_people, third_dose_people]
+                    
+                    
+                    
+                    ### ACTUAL GRAPH GEN PART ###
+                    
+                    
+                    # Subplots
+                    fig = make_subplots(
+                        rows=2, cols=2,
+                        shared_xaxes=True,
+                        vertical_spacing=0.03,
+                        column_widths=[1, 0.4],
+                        # row_heights=[0.2, 1],
+                        specs=[[{"secondary_y": True}, {"type": "pie"}],
+                               [{"type": "scatter"}, {"type": "choropleth"}]])
+                    
+                    fig.add_trace(
+                        go.Scatter(
+                            x=df_filtered["date"],
+                            y=df_filtered['cumCasesBySpecimenDate'],
+                            mode="lines",
+                            name="COVID-19 Cases",
+                        ),
+                        row=1, col=1
+                    )
+                    
+                    fig.add_trace(
+                        go.Scatter(
+                            x=df_filtered["date"], 
+                            y=df_filtered['newCasesBySpecimenDate'], 
+                            name="New COVID-19 Cases"),
+                        secondary_y=True,
+                        row=1, col=1
+                    )
+                    
+                    fig.add_trace(
+                        go.Scatter(
+                            x=df_filtered["date"],
+                            y=df_filtered['cumPeopleVaccinatedFirstDoseByVaccinationDate'],
+                            mode="lines",
+                            name="First Dose"
+                        ),
+                        row=2, col=1
+                    )
+                
+                    fig.add_trace(
+                        go.Scatter(
+                            x=df_filtered["date"], 
+                            y=df_filtered['cumPeopleVaccinatedSecondDoseByVaccinationDate'], 
+                            name="Second Dose"),
+                        row=2, col=1
+                    )
+                
+                    fig.add_trace(
+                        go.Scatter(
+                            x=df_filtered["date"], 
+                            y=df_filtered['cumPeopleVaccinatedThirdInjectionByVaccinationDate'], 
+                            name="Third Dose"),
+                        row=2, col=1
+                    )
+                    
+                    
+                
+                
+                
+                    fig.add_trace(
+                        go.Pie(
+                            labels=['First Dose','Second Dose', 'Third Injection'], 
+                            values=values, 
+                            textinfo='label+percent', 
+                            pull=[0,0,0],
+                            name="Vaccine Doses"
+                        ), 
+                        row=1, col=2
+                    )
+                    
+                    
+                    
+                    
+                    fig.add_trace(          # GEOGRAPHICAL MAP
+                        go.Choropleth(
+                            geojson = '/Users/vandam/Library/Mobile Documents/com~apple~CloudDocs/Documents/School/Year 1/Programming/Documents/Further Programming/FP-Assignment/Rough/lad.json',
+                            locationmode = 'geojson-id',
+                            # featureidkey='properties.LAD13NM',
+                            # locations=df_filtered["areaName"],
+                            # color=df_filtered["cumCasesBySpecimenDate"],
+                            # color_continuous_scale='Reds',
+                            # locationmode = 'geojson-id',
+                            # color='cumCasesBySpecimenDate',
+                            # color=df_filtered['cumCasesBySpecimenDate'],
+                            # hover_name=df_filtered["areaName"],
+                            # animation_frame=df_filtered["date"],
+                            name="COVID-19 Cases",
+                        ),
+                        row=2, col=2
+                    )
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    fig.update_yaxes(title_text="Cumulative COVID-19 Cases", row=1, col=1)
+                    fig.update_yaxes(title_text="New Cases", row=1, col=1, secondary_y=True)
+                    fig.update_yaxes(title_text="Vaccine Doses", row=2, col=1)
+                    
+                    fig.update_xaxes(title_text="", row=1, col=1)
+                    fig.update_xaxes(title_text="Date", row=2, col=1)
+                    
+                    fig.update_layout(title_text="COVID-19 Data")
+                    fig.update_layout(xaxis_range=[start_date, end_date])
+                    # fig.update_geos(scope='europe') 
+                    
+                    fig.update_layout(
+                        xaxis=dict(
+                            rangeselector=dict(
+                                buttons=list(
+                                    [
+                                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                                        dict(count=6, label="6m", step="month", stepmode="backward"),
+                                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                                        dict(step="all"),
+                                    ]
+                                )
+                            ),
+                            type="date",
+                        ),
+                        xaxis2=dict(
+                            rangeslider=dict(visible=True),
+                            type="date",
+                        ),
+                    )
+                    fig.update_xaxes(matches='x')
+                    fig.show()
+                    
+                
+                    
+                    
+                    
+                    
+                    # # Line Chart
+                    # fig1 = px.line(df_filtered, x = 'date', 
+                    #                y = ['cumCasesBySpecimenDate',
+                    #                     'cumPeopleVaccinatedFirstDoseByVaccinationDate', 
+                    #                     'cumPeopleVaccinatedSecondDoseByVaccinationDate',
+                    #                     'cumPeopleVaccinatedThirdInjectionByVaccinationDate'], 
+                    #                labels=dict(x="Date", y="Number of people"))
+                    # fig1.update_layout(xaxis_range=[start_date, end_date])
+                    # fig1.show()
+                    
+                    # # Pie Chart
+                    # first_dose_people = df_reset.loc[0,'cumPeopleVaccinatedFirstDoseByVaccinationDate']
+                    # second_dose_people = df_reset.loc[0,'cumPeopleVaccinatedSecondDoseByVaccinationDate']
+                    # third_dose_people = df_reset.loc[0,'cumPeopleVaccinatedThirdInjectionByVaccinationDate']
+                    # values = [first_dose_people, second_dose_people, third_dose_people]
+                            
+                    # fig2 = go.Figure(data=[go.Pie(labels=['First Dose','Second Dose', 'Third Injection'], values=values, textinfo='label+percent', pull=[0.1,0.1,0.1])])
+                    # fig2.show()
+                    
+                        
+                    #elif N > 1:
+                        #print('poop')
+            
 
 
 
@@ -339,7 +359,7 @@ if choice == 'l':
 if choice == 'c':
     LocationInput()
     timeRange()
-    graphgen(userLocationsList)
+    graphgen()
 
 
         
