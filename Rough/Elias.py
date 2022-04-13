56232b171eb8651b1901993e9a6263d643a7211c
Elias Momann <ua21653@bristol.ac.uk>
2022-04-13 15:55:39 +0100

    Using updated code from dash test and added Pie chart code

--- Rough/Elias.py
+++ Rough/Elias.py
@@ -1,131 +1,165 @@
+#Elias
 
-##### PACKAGES #####
 
-import pandas as pd
-import plotly.express as px
+# Misc
+import json
+import requests
+from datetime import date
+import re
 import plotly.io as pio
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
-pio.renderers.default = 'browser'           # Displays the graph in the Browser
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
 
 
-##### URLS #####
-
-# Spreadsheet for all COVID-19 Cases and Vaccine Doses for all areas in the UK
-url = 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&metric=cumCasesBySpecimenDate&metric=cumPeopleVaccinatedFirstDoseByVaccinationDate&metric=cumPeopleVaccinatedSecondDoseByVaccinationDate&metric=cumPeopleVaccinatedThirdInjectionByVaccinationDate&format=csv'
-url2 = 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&metric=cumPeopleVaccinatedFirstDoseByVaccinationDate&metric=cumPeopleVaccinatedSecondDoseByVaccinationDate&metric=cumPeopleVaccinatedThirdInjectionByVaccinationDate&metric=newCasesBySpecimenDate&metric=cumCasesBySpecimenDate&format=csv'
-
-# New Cases, Cumulative Cases, Vaccines
-url3 = 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&metric=cumCasesBySpecimenDate&metric=cumPeopleVaccinatedFirstDoseByVaccinationDate&metric=cumPeopleVaccinatedSecondDoseByVaccinationDate&metric=newCasesBySpecimenDate&metric=cumPeopleVaccinatedThirdInjectionByVaccinationDate&format=csv'
+polygons = requests.get(
+    "https://gist.githubusercontent.com/duhaime/1d6d5a8dc77c86128fcc1a05a72726c9/raw/8b8522cbc69498b6c4983a9f58c045c2b451cb89/british-isles-counties.geojson"
+).json()
 
 
-##### FUNCTIONS #####
-def UserChoice():     # Initial startup question
-    """
-      * The initial startup question for the user. 
-      - If the user does not enter one of the 3 valid inputs, an error message
-      - will appear and the input question will repeat until a correct input 
-      - is entered.
-    """
-    global choice
-    choice = input(
-        '\nPlease choose what to do:\n  l: Show list of cities\n  c: '
-        'Choose cities\n  q: Quit\n\n» Action: ')
-    while choice != 'l' and choice != 'c' and choice != 'q':
-        print('Error, please enter a valid input')
-        choice = input(
-            '\nPlease choose what to do:\n  l: Show list of cities\n  c: '
-            'Choose cities\n  q: Quit\n\n» Action: ')
+# URL of CSV
+url = 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&metric=cumCasesBySpecimenDate&metric=cumPeopleVaccinatedFirstDoseByVaccinationDate&metric=cumPeopleVaccinatedSecondDoseByVaccinationDate&metric=newCasesBySpecimenDate&metric=cumPeopleVaccinatedThirdInjectionByVaccinationDate&format=csv'
 
 
-# Shows list of locations
-def ShowList():
-    """
-      - Shows the list of locations
-      - Currently reads all the data in the areaName column of the csv file.
-      - Sorts into a list, then prints a set to remove duplicates
-    """
-    df = pd.read_csv(url)
-    areaList = df['areaName'].tolist()
-    print(set(areaList))
-    len(set(areaList))
+# Read CSV
+df = pd.read_csv(url)
+
+# Determining the maximum date range - YYYY-MM-DD
+dMin_value, dMax_value = df['date'].min(), df['date'].max()
 
 
-def LocationInput():
-    """
-      - Asks the user to input a location or multiple locations.
-      - Removes the spaces after each comma.
-      - Replaces the spaces between the words of two worded cities with |.
-        This allows us to find the exact match of the city later in the
-        graph gen function, instead of matching with other cities. This is 
-        common for cities that contain with 'North', 'South', 'East', 'West'
-        and 'and'.
-    """
-    global userLocationsList
-    userLocations = input(
-        "Please enter the location(s) you would like data for:\n  You can "
-        "enter a single location, or multiple locations seperated with commas."
-        "\n\n  Examples:\n  Bristol - Only data for Bristol will be shown.\n  "
-        "Liverpool, London - Data for both Liverpool and London will be shown."
-        "\n\n» Location(s): ")
-
-    # Removes the spaces after the commas
-    userLocations = userLocations.replace(", ", ",")
-    # Replaces the spaces between two-worded cities with |
-    userLocations = userLocations.replace(" ", "|")
-    # Splits cities into a list, where the user seperates the cities with commas
-    userLocationsList = userLocations.split(",")
+# BUG WITH DASH PLOTLY - Cannot select cities with commas
+# Renaming the cities with commas
+df.replace(to_replace="Armagh City, Banbridge and Craigavon",
+           value="Armagh City and Banbridge and Craigavon",
+           inplace=True)
+df.replace(to_replace="Bristol, City of",
+           value="Bristol",
+           inplace=True)
+df.replace(to_replace="Bournemouth, Christchurch and Poole",
+           value="Bournemouth and Christchurch and Poole",
+           inplace=True)
+df.replace(to_replace="Herefordshire, County of",
+           value="Herefordshire",
+           inplace=True)
+df.replace(to_replace="Kingston upon Hull, City of",
+           value="Kingston upon Hull",
+           inplace=True)
+df.replace(to_replace="Newry, Mourne and Down",
+           value="Newry and Mourne and Down",
+           inplace=True)
 
 
-def timeRange():
-    global start_date
-    global end_date
-
-    df = pd.read_csv(url3)
-    df_filtered = df[df['areaName'].str.contains(
-        r'\b' + userLocationsList[0] + r'\b')]
-    min_value, max_value = df_filtered['date'].min(), df_filtered['date'].max()
-
-    # WORK IN PROGRESS - tells the user which locations they have selected. e.g. if 'Bristol' is typed, this will print 'Bristol, City of'
-    # currently just repeats the users input
-    print('Analysing data for the following location(s):\n' + str(userLocationsList))
-
-    print('\nPlease select a time range:\n  Possible start date:', min_value,
-          '\n  Possible end date:', max_value, '\n\n  Examples:\n  All - '
-          'this will display data across the whole time range.\n  YYYY-MM-DD'
-          ' - YYYY-MM-DD - This will display data for the specified dates.')
-    DateInput = input("» Time Range: ")
-
-    if DateInput == "All":
-        start_date = min_value
-        end_date = max_value
-    elif DateInput != "All":
-        DateInput = DateInput.replace(' - ', ',')
-        # DateInput = DateInput.replace('/', '-')
-        DateInput = DateInput.split(",")
-        start_date = DateInput[0]
-        end_date = DateInput[1]
-
-    # print(start_date, end_date)
+# City list
+areaList = sorted(list(set(df['areaName'].tolist())))
 
 
-# Generates Graph
-def graphgen(userLocationsList):
-    """
-      - Determines the number of cities entered
-      - If there is one city entered, generates a graph of covid cases
-      - If there is more than one city entered, poop is outputted
-    """
-    # The number of words in userLocations
-    N = len(userLocationsList)
+# Formatting style for City and Date Input
+style_dict = dict(width='100%',
+                  # border='1.5px black solid',
+                  height='50px',
+                  # textAlign='center',
+                  fontFamily='HelveticaNeue',
+                  fontSize=20)
 
-    # Instance for 1 Location inputted
-    if N == 1:
-        df = pd.read_csv(url3)
-        df_filtered = df[df['areaName'].str.contains(
-            r'\b' + userLocationsList[0] + r'\b')]
+# Formatting for Header and Description
+style_dict1 = dict(
+    # border='1.5px black solid',
+    # height='50px',
+    textAlign='center',
+    fontFamily='HelveticaNeue',
+)
+
+
+app.layout = html.Div(children=[
+    html.H1(children='COVID-19 Data', style=style_dict1),
+
+    html.Div(children='A web application for COVID-19 Data using Dash Plotly.'
+             ' Made by Bayan, Elias, Margherita and Vandam.',
+             style=style_dict1),
+
+    # Placeholder
+    html.Div(style={'width': '2%', 'display': 'inline-block'}),
+
+    # Placeholder
+    html.Div(style={'width': '50%', 'display': 'flex'}),
+
+    # City Selection Dropdown
+    html.Div(
+        dcc.Dropdown(
+            areaList,
+            ['Bristol'],
+            id='city-dropdown',
+            multi=True,
+            style=style_dict,
+        ),
+        style={'width': '40%', 'display': 'flex', 'vertical-align': 'top',
+               'margin-left': 'auto', 'margin-right': 'auto'}
+    ),
+
+    # Placeholder
+    html.Div(style={'width': '2%', 'display': 'inline-block',
+             'vertical-align': 'top'}),
+
+    # Date Range Picker
+    html.Div(
+        dcc.DatePickerRange(
+            id='date-range-picker',
+            min_date_allowed=date(int(dMin_value[0:4]), int(
+                dMin_value[5:7]), int(dMin_value[8:10])),
+            max_date_allowed=date(int(dMax_value[0:4]), int(
+                dMax_value[5:7]), int(dMax_value[8:10])),
+            start_date=date(int(dMin_value[0:4]), int(
+                dMin_value[5:7]), int(dMin_value[8:10])),
+            end_date=date(int(dMax_value[0:4]), int(
+                dMax_value[5:7]), int(dMax_value[8:10])),
+            style=style_dict,
+        ),
+        style={'width': '40%', 'display': 'flex', 'vertical-align': 'top',
+               'margin-left': 'auto', 'margin-right': 'auto'}
+    ),
+
+    # Output Main Graph
+    dcc.Graph(
+        id='final-graph'
+    ),
+
+    # Output Choropleth Map with loading animation
+    dcc.Loading(
+        id="loading-1",
+        children=[dcc.Graph(id="map-graph")],
+        type="circle",
+    )
+
+])
+
+
+# When City and dates selected, graph updates with specified values
+@app.callback(
+    Output('final-graph', 'figure'),
+    Input('city-dropdown', 'value'),
+    Input('date-range-picker', 'start_date'),
+    Input('date-range-picker', 'end_date')
+)
+def update_output(city, start_date, end_date):
+    N = len(city)
+
+    fig = make_subplots(
+        rows=2, cols=2,
+        shared_xaxes=True,
+        vertical_spacing=0.03,
+        column_widths=[1, 0.4],
+        # row_heights=[0.2, 1],
+        specs=[[{"secondary_y": True}, {"type": "pie"}],
+               [{"type": "scatter"}, {"type": "choropleth"}]])
+
+    for i in range(N):
+        df_filtered = df[df['areaName'].str.contains(r'\b' + city[i] + r'\b')]
         df_reset = df_filtered.reset_index(drop=True)
-
         first_dose_people = df_reset.loc[0,
                                          'cumPeopleVaccinatedFirstDoseByVaccinationDate']
         second_dose_people = df_reset.loc[0,
@@ -134,24 +168,12 @@ def graphgen(userLocationsList):
                                          'cumPeopleVaccinatedThirdInjectionByVaccinationDate']
         values = [first_dose_people, second_dose_people, third_dose_people]
 
-        ### ACTUAL GRAPH GEN PART ###
-
-        # Subplots
-        fig = make_subplots(
-            rows=2, cols=2,
-            shared_xaxes=True,
-            vertical_spacing=0.03,
-            column_widths=[1, 0.4],
-            # row_heights=[0.2, 1],
-            specs=[[{"secondary_y": True}, {"type": "pie"}],
-                   [{"type": "scatter"}, None]])
-
         fig.add_trace(
             go.Scatter(
                 x=df_filtered["date"],
                 y=df_filtered['cumCasesBySpecimenDate'],
                 mode="lines",
-                name="COVID-19 Cases"
+                name="Total Cases for " + str(city[i])
             ),
             row=1, col=1
         )
@@ -160,7 +182,7 @@ def graphgen(userLocationsList):
             go.Scatter(
                 x=df_filtered["date"],
                 y=df_filtered['newCasesBySpecimenDate'],
-                name="New COVID-19 Cases"),
+                name="New Cases for " + str(city[i])),
             secondary_y=True,
             row=1, col=1
         )
@@ -170,7 +192,7 @@ def graphgen(userLocationsList):
                 x=df_filtered["date"],
                 y=df_filtered['cumPeopleVaccinatedFirstDoseByVaccinationDate'],
                 mode="lines",
-                name="First Dose"
+                name="First Dose for " + str(city[i])
             ),
             row=2, col=1
         )
@@ -179,7 +201,8 @@ def graphgen(userLocationsList):
             go.Scatter(
                 x=df_filtered["date"],
                 y=df_filtered['cumPeopleVaccinatedSecondDoseByVaccinationDate'],
-                name="Second Dose"),
+                name="Second Dose for " + str(city[i])
+            ),
             row=2, col=1
         )
 
@@ -187,10 +210,10 @@ def graphgen(userLocationsList):
             go.Scatter(
                 x=df_filtered["date"],
                 y=df_filtered['cumPeopleVaccinatedThirdInjectionByVaccinationDate'],
-                name="Third Dose"),
+                name="Third Dose for " + str(city[i])
+            ),
             row=2, col=1
         )
-        # marker_colors = ['#00CC96', 'AB63FA', 'FFA15A']
         colors = ['green', 'purple', 'orange']
         fig.add_trace(
             go.Pie(
@@ -198,53 +221,85 @@ def graphgen(userLocationsList):
                 values=values,
                 textinfo='label+percent',
                 marker=dict(
-                    colors=colors, line=dict(color='#000000', width=0.2)),
+                   colors=colors, line=dict(color='#000000', width=0.2)),
                 pull=[0, 0, 0],
                 name="Vaccine Doses"),
             row=1, col=2)
-
         
 
-        fig.update_yaxes(title_text="Cumulative COVID-19 Cases", row=1, col=1)
-        fig.update_yaxes(title_text="New Cases", row=1,
-                         col=1, secondary_y=True)
-        fig.update_yaxes(title_text="Vaccine Doses", row=2, col=1)
+    fig.update_geos(fitbounds="locations")
+    fig.update_yaxes(title_text="Cumulative COVID-19 Cases", row=1, col=1)
+    fig.update_yaxes(title_text="New Cases", row=1, col=1, secondary_y=True)
+    fig.update_yaxes(title_text="Vaccine Doses", row=2, col=1)
 
-        fig.update_xaxes(title_text="", row=1, col=1)
-        fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_xaxes(title_text="", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_layout(xaxis_range=[start_date, end_date])
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list(
+                    [
+                        dict(count=1, label="1m", step="month",
+                             stepmode="backward"),
+                        dict(count=6, label="6m", step="month",
+                             stepmode="backward"),
+                        dict(count=1, label="1y", step="year",
+                             stepmode="backward"),
+                        dict(step="all"),
+                    ]
+                )
+            ),
+            type="date",
+        ),
+        xaxis2=dict(
+            rangeslider=dict(visible=True),
+            type="date",
+        ),
+    )
+    fig.update_xaxes(matches='x')
+    fig.update_layout(title_text="COVID-19 Data")
+    fig.update_layout(xaxis_range=[start_date, end_date])
+    fig.update_layout(height=1000, autosize=True)
 
-        fig.update_layout(title_text="COVID-19 Data")
-        fig.update_layout(xaxis_range=[start_date, end_date])
-        fig.show()
-
-        # # Line Chart
-        # fig1 = px.line(df_filtered, x = 'date',
-        #                y = ['cumCasesBySpecimenDate',
-        #                     'cumPeopleVaccinatedFirstDoseByVaccinationDate',
-        #                     'cumPeopleVaccinatedSecondDoseByVaccinationDate',
-        #                     'cumPeopleVaccinatedThirdInjectionByVaccinationDate'],
-        #                labels=dict(x="Date", y="Number of people"))
-        # fig1.update_layout(xaxis_range=[start_date, end_date])
-        # fig1.show()
-
-        # # Pie Chart
-        # first_dose_people = df_reset.loc[0,'cumPeopleVaccinatedFirstDoseByVaccinationDate']
-        # second_dose_people = df_reset.loc[0,'cumPeopleVaccinatedSecondDoseByVaccinationDate']
-        # third_dose_people = df_reset.loc[0,'cumPeopleVaccinatedThirdInjectionByVaccinationDate']
-        # values = [first_dose_people, second_dose_people, third_dose_people]
-
-        # fig2 = go.Figure(data=[go.Pie(labels=['First Dose','Second Dose', 'Third Injection'], values=values, textinfo='label+percent', pull=[0.1,0.1,0.1])])
-        # fig2.show()
-
-    elif N > 1:
-        print('poop')
+    return fig
 
 
-##### TEST AREA #####
-UserChoice()
-if choice == 'l':
-    ShowList()
-if choice == 'c':
-    LocationInput()
-    timeRange()
-    graphgen(userLocationsList)
+# Ouput Choropleth graph - doesn't take any inputs currently
+@app.callback(
+    Output('map-graph', 'figure'),
+    Input('date-range-picker', 'start_date'),
+    Input('date-range-picker', 'end_date')
+)
+def update_map(start_date, end_date):
+    df = pd.read_csv(url)
+    df["date2"] = pd.to_datetime(df['date'])
+    df = df[df['date2'].dt.day == 1]
+
+    fig1 = px.choropleth_mapbox(
+        df.iloc[::-1],
+        geojson=polygons,
+        locations='areaName',
+        featureidkey="properties.NAME_2",
+        color='newCasesBySpecimenDate',
+        color_continuous_scale="Reds",
+        range_color=(0, 500),
+        hover_name='areaName',
+        labels={"newCasesBySpecimenDate": "Cases"},
+        animation_frame="date",
+        animation_group="areaCode",
+        center={"lat": 54.768483, "lon": -4.417318},
+        mapbox_style="carto-positron", zoom=5
+    )
+
+    fig1.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig1.update_geos(fitbounds="geojson", visible=False)
+    fig1.update_layout(height=1000, autosize=True)
+    # fig.show()
+    return fig1
+
+
+# Testing
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
