21482c69dd7e21fb28773df3dc1e1372d227c5e9
Vandam Dinh <vandamdinh71@gmail.com>
2022-04-27 00:19:33 +0100

    Update app.py for gunicorn

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ import plotly.express as px
 import requests
 import json
 
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
